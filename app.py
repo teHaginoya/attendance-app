48b41e0e882927fd86bdd878c7a8c0bfc958a134
teHaginoya <it.te.haginoya@iftc.co.jp>
2025-12-11 13:44:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,12 @@ st.markdown("""
     }
     
     /* 削除確認ダイアログ用のカラム設定（st.columnsを使用） */
+    [data-testid="stLayoutWrapper"] {
+        max-width: 93% !important;
+        margin-left: auto !important;
+        margin-right: auto !important;
+    }
+
     [data-testid="column"] {
     }
     
