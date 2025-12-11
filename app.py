792cd72a7d3ceedd9650da1fe7911f7dce29de6d
teHaginoya <it.te.haginoya@iftc.co.jp>
2025-12-11 14:29:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
-from datetime import datetimefda
+from datetime import datetime
 import time
 
 # ページ設定
